c893c3848e2d9822b8bfcd10e9befae003d182a1
aelaraby <body1262005@gmail.com>
2025-01-23 21:16:55 +0200

    new commands

--- main.cpp
+++ main.cpp
@@ -28,6 +28,16 @@ int main() {
         }
         else if (newCommand.name == "pwd"){
             cout<<currentDirectory<<endl;
+        } else if(newCommand.name == "touch"){
+            createFile(newCommand.parameters,newCommand.options);
+        } else if(newCommand.name == "cat"){
+            ReadFileContent(newCommand.parameters,newCommand.options);
+        } else if(newCommand.name == "rm"){
+            DeleteFile(newCommand.parameters,newCommand.options);
+        } else if(newCommand.name == "mv"){
+           MoveFile(newCommand.parameters,newCommand.options);
+        } else if(newCommand.name == "cp"){
+           CopyFile(newCommand.parameters,newCommand.options);
         }
     }
 }
